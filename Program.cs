22b815f629946e099a99df7e404148165cf9f3ce
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-14 17:01:32 -1200

    Welcome page

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@
     {
         public static void Main(string[] args) 
         {
-         
-            PrimeFactors.PrimeFactor();
+            Console.WriteLine("Welcome to Basic Core Programs");
+            //HarmonicNumber.Harm();
+            //VowelsAndConsonants.CheckVovelAndConsonant();
+            //SwapTwoNumbers.DisplaySwapTwoNumbers();
+            //LargestNumber.FineLargestNumber();
+            //QuontientAndReminder.FindQuontientAndReminder();
+            //EvenOrOdd.FineEvenOrOdd();
+            //FlipCoin.FlipTheCoin();
+            //LeapYear.Leapyear();
+            PowerOf2.Power2();
         }
     }
 }
